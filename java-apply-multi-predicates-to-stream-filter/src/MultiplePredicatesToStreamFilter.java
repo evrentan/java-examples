9f97cde47f3aea027f95c62552c0f5328c33ae3b
Evren Tan <evrentan@gmail.com>
2022-03-24 12:04:46 +0300

    feat: Refactor Multi Predicates
    Refactor Multi Predicates

--- java-apply-multi-predicates-to-stream-filter/src/MultiplePredicatesToStreamFilter.java
+++ java-apply-multi-predicates-to-stream-filter/src/MultiplePredicatesToStreamFilter.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 public class MultiplePredicatesToStreamFilter {
 
+  private static final List<Integer> myIntegerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
   public static void main(String[] args) {
-    List<Integer> myIntegerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     Predicate<Integer> isEven = i -> i % 2 == 0;
     Predicate<Integer> isGreaterThan5 = i -> i > 5;
@@ -15,19 +16,40 @@ public class MultiplePredicatesToStreamFilter {
     Predicate<Integer> myPredicatesWithOr = myPredicateList.stream().reduce(Predicate::or).orElse(p -> false);
     Predicate<Integer> myPredicatesWithAnd = myPredicateList.stream().reduce(Predicate::and).orElse(p -> true);
 
-    final String myEvenIntegersOrGreaterThan5 = myIntegerList.stream().
-        filter(myPredicatesWithOr).
-        map(Object::toString).
-        collect(Collectors.joining(", "));
+    final String myEvenIntegersOrGreaterThan5WithReducedPredicates = myIntegerList.stream()
+        .filter(myPredicatesWithOr)
+        .map(Object::toString)
+        .collect(Collectors.joining(", "));
 
-    final String myEvenIntegersAndGreaterThan5 = myIntegerList.stream().
-        filter(myPredicatesWithAnd).
-        map(Object::toString).
-        collect(Collectors.joining(", "));
+    final String myEvenIntegersOrGreaterThan5WithPredicateOperators = myIntegerList.stream()
+        .filter(isEven.or(isGreaterThan5))
+        .map(Object::toString)
+        .collect(Collectors.joining(", "));
 
-    System.out.println(String.format("Even Integers or Integers Greater than 5 are; %s", myEvenIntegersOrGreaterThan5));
+    final String myEvenIntegersAndGreaterThan5WithReducedPredicates = myIntegerList.stream()
+        .filter(myPredicatesWithAnd)
+        .map(Object::toString)
+        .collect(Collectors.joining(", "));
+
+    final String myEvenIntegersAndGreaterThan5WithPredicateOperators = myIntegerList.stream()
+        .filter(isEven.and(isGreaterThan5))
+        .map(Object::toString)
+        .collect(Collectors.joining(", "));
+
+    final String myOddIntegersAndSmallerThanOrEqualTo5WithPredicateOperators = myIntegerList.stream()
+        .filter(isEven.negate().and(isGreaterThan5.negate()))
+        .map(Object::toString)
+        .collect(Collectors.joining(", "));
+
+    System.out.println(String.format("Even Integers or Integers Greater than 5 are with reduced predicates; %s", myEvenIntegersOrGreaterThan5WithReducedPredicates));
     //  Even Integers or Integers Greater Than 5 are; 2, 4, 6, 7, 8, 9, 10
-    System.out.println(String.format("Even Integers and Integers Greater than 5 are; %s", myEvenIntegersAndGreaterThan5));
+    System.out.println(String.format("Even Integers or Integers Greater than 5 are with predicate operators; %s", myEvenIntegersOrGreaterThan5WithPredicateOperators));
+    //  Even Integers or Integers Greater than 5 are with predicate operators; 2, 4, 6, 7, 8, 9, 10
+    System.out.println(String.format("Even Integers and Integers Greater than 5 are with reduced predicates; %s", myEvenIntegersAndGreaterThan5WithReducedPredicates));
     // Even Integers and Integers Greater Than 5 are; 6, 8, 10
+    System.out.println(String.format("Even Integers and Integers Greater than 5 are with predicate operators; %s", myEvenIntegersAndGreaterThan5WithPredicateOperators));
+    // Even Integers and Integers Greater than 5 are with predicate operators; 6, 8, 10
+    System.out.println(String.format("Odd Integers and Integers Smaller than or Equal to 5 are with predicate operators; %s", myOddIntegersAndSmallerThanOrEqualTo5WithPredicateOperators));
+    // Odd Integers and Integers Smaller than or Equal to 5 are with predicate operators; 1, 3, 5
   }
 }
