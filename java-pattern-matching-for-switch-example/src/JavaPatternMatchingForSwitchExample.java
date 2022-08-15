cc68bebfc450c99a472488b0ff314f63c4cdf39c
Evren Tan <evrentan@gmail.com>
2022-08-15 11:15:49 +0300

    Refactor JavaPatternMatchingForSwitchExample

--- java-pattern-matching-for-switch-example/src/JavaPatternMatchingForSwitchExample.java
+++ java-pattern-matching-for-switch-example/src/JavaPatternMatchingForSwitchExample.java
@@ -9,7 +9,7 @@ public class JavaPatternMatchingForSwitchExample {
 
     List<Object> myObjectList = new ArrayList<>();
 
-    javaPatternMatchingForSwitchExample.generateObjectList(myObjectList);
+    javaPatternMatchingForSwitchExample.initializeInputs(myObjectList);
 
     javaPatternMatchingForSwitchExample.checkObjectListTypePatternWithInstanceOf(myObjectList);
     javaPatternMatchingForSwitchExample.checkObjectListTypePatternWithSwitch(myObjectList);
@@ -17,7 +17,14 @@ public class JavaPatternMatchingForSwitchExample {
     System.out.println("*********************");
   }
 
-  private void generateObjectList(List<Object> myObjectList) {
+  /**
+   * Initialize inputs for the application
+   *
+   * @param myObjectList Empty object list to be filled from command prompt inputs
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   */
+  private void initializeInputs(List<Object> myObjectList) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("*********************");
     System.out.println("This is an example Java app of how pattern matching for switch works");
@@ -29,6 +36,14 @@ public class JavaPatternMatchingForSwitchExample {
     System.out.println("*********************");
   }
 
+  /**
+   * Check Object List Type Pattern with instanceOf
+   * Call {@link #checkObjectTypePatternWithInstanceOf(Object) checkObjectTypePatternWithInstanceOf} method to check objects one by one using instanceOf
+   *
+   * @param objectList List of objects that is going to be checked for type pattern
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   */
   private void checkObjectListTypePatternWithInstanceOf (List<Object> objectList) {
     System.out.println("*********************");
     System.out.println("Checking Object List Type Pattern with instanceOf");
@@ -36,6 +51,13 @@ public class JavaPatternMatchingForSwitchExample {
     System.out.println("*********************");
   }
 
+  /**
+   * Check Object Type Pattern with instanceOf
+   *
+   * @param o Object that is going to be checked for type pattern
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   */
   private void checkObjectTypePatternWithInstanceOf (Object o) {
     if (o instanceof String s)
       System.out.println(String.format("%s is a String with length %d !!!", s, s.length()));
@@ -45,6 +67,14 @@ public class JavaPatternMatchingForSwitchExample {
       System.out.println("Object is not Pre-Defined !!!");
   }
 
+  /**
+   * Check Object List Type Pattern with Switch
+   * Call {@link #checkObjectTypePatternWithSwitch(Object) checkObjectTypePatternWithSwitch} method to check objects one by one using switch
+   *
+   * @param objectList List of objects that is going to be checked for type pattern
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   */
   private void checkObjectListTypePatternWithSwitch (List<Object> objectList) {
     System.out.println("*********************");
     System.out.println("Checking Object List Type Pattern with Switchth");
@@ -52,6 +82,13 @@ public class JavaPatternMatchingForSwitchExample {
     System.out.println("*********************");
   }
 
+  /**
+   * Check Object Type Pattern with switch
+   *
+   * @param o Object that is going to be checked for type pattern
+   *
+   * @author <a href="https://github.com/evrentan">Evren Tan</a>
+   */
   private void checkObjectTypePatternWithSwitch (Object o) {
     switch (o) {
       case String s -> System.out.println(String.format("%s is a String with length %d !!!", s, s.length()));
