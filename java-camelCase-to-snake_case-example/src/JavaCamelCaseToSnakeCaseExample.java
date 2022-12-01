efb9d476b2318913cdb4063a368507f14b454075
evrentan <evrentan@gmail.com>
2022-12-01 15:25:38 +0300

    Create camelCase to snake_case example
    Create camelCase to snake_case example

---
+++ java-camelCase-to-snake_case-example/src/JavaCamelCaseToSnakeCaseExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class JavaCamelCaseToSnakeCaseExample {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter a string in camelCase format: ");
+    String myString = scanner.nextLine();
+    scanner.close();
+
+    JavaCamelCaseToSnakeCaseExample javaCamelCaseToSnakeCaseExample = new JavaCamelCaseToSnakeCaseExample();
+
+    System.out.println(javaCamelCaseToSnakeCaseExample.camelToUnderscore(myString));
+
+  }
+
+  public String camelToUnderscore(String input) {
+    if (input == null) {
+      return "";
+    }
+    String regex = "([a-z])([A-Z])";
+    String replacement = "$1_$2";
+    String result = input.replaceAll(regex, replacement).toLowerCase();
+    if (result.startsWith("_"))
+      return result.substring(1);
+    return result;
+  }
+}
